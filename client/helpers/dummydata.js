1029b4b5bca175d988d0077c06375d8b9d76ace3
jonatso <jonatan.solheim@ntnu.no>
2022-02-10 11:11:46 +0100

    make cards ft sjbjerke <sjbjerke@ntnu.no>

---
+++ client/helpers/dummydata.js
@@ -0,0 +1,92 @@
+const dummyRecipes = [
+    {
+        id: 1,
+        name: "Pizza",
+        ingredients: [
+            {
+                id: 1,
+                name: "Dough",
+                quantity: "1",
+                unit: "kg",
+            },
+            {
+                id: 2,
+                name: "Tomato",
+                quantity: "1",
+                unit: "kg",
+            },
+            {
+                id: 3,
+                name: "Cheese",
+                quantity: "1",
+                unit: "kg",
+            },
+        ],
+        description: "This pizza is delicious",
+    },
+    {
+        id: 2,
+        name: "Burger",
+        ingredients: [
+            {
+                id: 1,
+                name: "Dough",
+                quantity: "1",
+                unit: "kg",
+            },
+
+            {
+                id: 2,
+                name: "Tomato",
+                quantity: "1",
+                unit: "kg",
+            },
+            {
+                id: 3,
+                name: "Cheese",
+                quantity: "1",
+                unit: "kg",
+            },
+        ],
+        description: "This burger is the best. My favourite recipe",
+    },
+    {
+        id: 3,
+        name: "Pasta",
+        ingredients: [
+            {
+                id: 1,
+                name: "Dough",
+                quantity: "1",
+                unit: "kg",
+            },
+            {
+                id: 2,
+                name: "Tomato",
+                quantity: "1",
+                unit: "kg",
+            },
+            {
+                id: 3,
+                name: "Cheese",
+                quantity: "1",
+                unit: "kg",
+            },
+            {
+                id: 4,
+                name: "Pasta",
+                quantity: "1",
+                unit: "kg",
+            },
+            {
+                id: 5,
+                name: "Salt",
+                quantity: "1",
+                unit: "kg",
+            },
+        ],
+        description: "This pasta is delicious with EXTRA dough!"
+        }
+]
+
+module.exports = dummyRecipes;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,100 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Box } from "@chakra-ui/react";
 import RecipeCard from "../components/recipecard";
+import dummyRecipes from "../helpers/dummydata";
 
 export default function Home() {
 	const [data, setData] = useState(null);
 	const [isLoading, setLoading] = useState(false);
-	const [recipes, setRecipes] = useState([
-		{
-			id: 1,
-			name: "Pizza",
-			ingredients: [
-				{
-					id: 1,
-					name: "Dough",
-					quantity: "1",
-					unit: "kg",
-				},
-				{
-					id: 2,
-					name: "Tomato",
-					quantity: "1",
-					unit: "kg",
-				},
-				{
-					id: 3,
-					name: "Cheese",
-					quantity: "1",
-					unit: "kg",
-				},
-			],
-			description: "This pizza is delicious",
-		},
-		{
-			id: 2,
-			name: "Burger",
-			ingredients: [
-				{
-					id: 1,
-					name: "Dough",
-					quantity: "1",
-					unit: "kg",
-				},
-
-				{
-					id: 2,
-					name: "Tomato",
-					quantity: "1",
-					unit: "kg",
-				},
-				{
-					id: 3,
-					name: "Cheese",
-					quantity: "1",
-					unit: "kg",
-				},
-			],
-			description: "This burger is the best. My favourite recipe",
-		},
-		{
-			id: 3,
-			name: "Pasta",
-			ingredients: [
-				{
-					id: 1,
-					name: "Dough",
-					quantity: "1",
-					unit: "kg",
-				},
-				{
-					id: 2,
-					name: "Tomato",
-					quantity: "1",
-					unit: "kg",
-				},
-				{
-					id: 3,
-					name: "Cheese",
-					quantity: "1",
-					unit: "kg",
-				},
-				{
-					id: 4,
-					name: "Pasta",
-					quantity: "1",
-					unit: "kg",
-				},
-				{
-					id: 5,
-					name: "Salt",
-					quantity: "1",
-					unit: "kg",
-				},
-			],
-			description: "This pasta is delicious with EXTRA dough!"
-			}
-	]);
+	const [recipes, setRecipes] = useState(dummyRecipes);
 
 	
 	return (
