1291e7943cc83ebdf95b12c81efcab3fb01d4b4d
evenasl <evenasl@stud.ntnu.no>
2022-02-10 09:35:05 +0100

    Cors setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,17 @@
 require("dotenv-safe").config();
 const express = require("express");
 const pgp = require("pg-promise")(/* options */);
+const cors = require("cors");
 const db = pgp(process.env.DATABASE_URL);
 
 const app = express();
+app.use(express.json());
+app.use(
+	cors({
+		origin: [process.env.CORS_ORIGIN_URL],
+	})
+);
+
 
 app.get("/", (req, res) => {
 	res
