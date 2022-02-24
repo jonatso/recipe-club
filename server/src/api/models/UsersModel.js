89ce1821f656745aa18a069fa03529cb74ffe8b6
EvenAsl <85791946+EvenAsl@users.noreply.github.com>
2022-02-24 12:49:46 +0100

    Update based on sequelize docs

--- server/src/api/models/UsersModel.js
+++ server/src/api/models/UsersModel.js
@@ -9,11 +9,13 @@ module.exports = (sequelize, DataTypes) => {
 			type: DataTypes.STRING,
 			allowNull: false,
 			unique: true,
+			validate: { isAlphanumeric: true },
 		},
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			unique: true,
+			validate: { isEmail: true },
 		},
 		password: {
 			type: DataTypes.STRING,
--- server/src/api/validations/UsersValidator.js
+++ server/src/api/validations/UsersValidator.js
@@ -1,15 +1,5 @@
-const emailValidator = require("email-validator");
-const isAlphanumeric = require("is-alphanumeric");
-
 const validateInput = (body) => {
-	if (!isAlphanumeric(body.username)) {
-		throw new Error("Username must be alphanumeric");
-	}
-	if (!emailValidator.validate(body.email)) {
-		throw new Error("email is not on correct format");
-	}
-
-	if (body.password == undefined || body.password.length < 3) {
+	if (body.password.length < 3) {
 		throw new Error("password is too short");
 	}
 };
