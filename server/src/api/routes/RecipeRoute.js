fc3a8e2dc76e95fa86334192b63f1bcb8fcf8dd9
EvenAsl <85791946+EvenAsl@users.noreply.github.com>
2022-03-01 00:16:29 +0100

    MergeProblemFixPart2

---
+++ server/src/api/routes/RecipeRoute.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const { RecipeController } = require("../controllers");
+const { isAuth } = require("../middlewares");
+
+const router = Router();
+
+router.get("/", RecipeController.getRecipes);
+
+router.get("/:id", RecipeController.getRecipe);
+
+router.use("/create", isAuth);
+router.post("/create", RecipeController.createRecipe);
+
+router.use("update/:id", isAuth);
+router.put("/update/:id", RecipeController.updateRecipe);
+
+router.use("/delete/:id", isAuth);
+router.delete("/delete/:id", RecipeController.deleteRecipe);
+
+module.exports = router;
