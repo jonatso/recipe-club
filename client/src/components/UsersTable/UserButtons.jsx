d1298187582210b05206814be9bb408c567c8113
evenasl <evenasl@stud.ntnu.no>
2022-03-01 10:56:39 +0100

    UsersTable

---
+++ client/src/components/UsersTable/UserButtons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+export default function UserButtons() {
+   return (
+      <>
+         <Button mr={2}>Edit</Button>
+         <Button>Delete</Button>
+      </>
+   );
+}
