6b823b8cf805a196c3343452e7f6e4e61a127089
Daniel King <danieki@ntnu.no>
2022-03-15 11:26:49 +0100

    Fixed search frontend
    Co-authored-by: Sunniva Bjerke <sjbjerke@ntnu.no>

--- client/src/components/navbar/Search.jsx
+++ client/src/components/navbar/Search.jsx
@@ -1,25 +1,13 @@
-import React, { useState } from "react";
-// import { SearchIcon } from "@chakra-ui/icons";
-// import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-
-// export default function Search() {
-//    const [value, setValue] = React.useState("");
-//    const handleChange = (event) => setValue(event.target.value);
-//    return (
-//       <InputGroup>
-//          <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />} />
-//          <Input value={value} onChange={handleChange} type="search" placeholder="Search for recipe" />
-//       </InputGroup>
-//    );
-// }
+import React from "react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 const SearchBar = () => (
    <form action="/" method="get">
-      <label htmlFor="header-search">
-         <span className="visually-hidden">Search blog posts</span>
-      </label>
-      <input type="text" id="header-search" placeholder="Search blog posts" name="q" />
-      <button type="submit">Search</button>
+      <InputGroup>
+         <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />} />
+         <Input type="search" placeholder="Search for recipe" name="q" />
+      </InputGroup>
    </form>
 );
 
