c307259fd439a44bfcb218663c15b4ee9454a61c
Daniel King <danieki@ntnu.no>
2022-03-15 11:09:17 +0100

    Fixed searchbar
    Co-authored-by: Sunniva Bjerke <sjbjerke@ntnu.no>

--- client/src/components/navbar/Search.jsx
+++ client/src/components/navbar/Search.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import { SearchIcon } from "@chakra-ui/icons";
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React, { useState } from "react";
+// import { SearchIcon } from "@chakra-ui/icons";
+// import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
-export default function Search() {
-   return (
-      <InputGroup>
-         <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />} />
-         <Input type="search" placeholder="Search for recipe" />
-      </InputGroup>
-   );
-}
+// export default function Search() {
+//    const [value, setValue] = React.useState("");
+//    const handleChange = (event) => setValue(event.target.value);
+//    return (
+//       <InputGroup>
+//          <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />} />
+//          <Input value={value} onChange={handleChange} type="search" placeholder="Search for recipe" />
+//       </InputGroup>
+//    );
+// }
+
+const SearchBar = () => (
+   <form action="/" method="get">
+      <label htmlFor="header-search">
+         <span className="visually-hidden">Search blog posts</span>
+      </label>
+      <input type="text" id="header-search" placeholder="Search blog posts" name="q" />
+      <button type="submit">Search</button>
+   </form>
+);
+
+export default SearchBar;
