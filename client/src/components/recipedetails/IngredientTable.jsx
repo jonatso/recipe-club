4159b4452f3a5e987f92c437a86d980c4738f528
jonatso <jonatan.solheim@ntnu.no>
2022-05-24 12:37:58 +0200

    reintroduce change portion amount

--- client/src/components/recipedetails/IngredientTable.jsx
+++ client/src/components/recipedetails/IngredientTable.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { Box, Text, Thead, Tr, Th, Td, Tbody, Table, useColorModeValue } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Text, Thead, Tr, Th, Td, Tbody, Table, useColorModeValue, HStack, IconButton } from "@chakra-ui/react";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 export default function IngredientTable({ ingredients }) {
+   const [numPortions, setNumPortions] = useState(4);
+   
    return (
       <Box>
          <Text
@@ -13,6 +16,38 @@ export default function IngredientTable({ ingredients }) {
          >
             Ingredients
          </Text>
+         <HStack 
+                  bg={useColorModeValue('gray.200', 'gray.600')}
+                  width='fit-content'
+                  padding='1'
+                  borderRadius='md'
+                >
+                  <IconButton 
+                    colorScheme='teal' 
+                    icon={<FaMinus />} 
+                    onClick={() => setNumPortions(numPortions - 1)}
+                    isRound
+                  />
+                  <Text 
+                    fontSize={'lg'} 
+                    bg={useColorModeValue('white', 'gray.800')} 
+                    pl='6' 
+                    pr='6'
+                    pt='2'
+                    pb='2'
+                    borderRadius='md'
+                    color={useColorModeValue('gray.800', 'white')}
+                  >
+                    {numPortions}
+                  </Text>
+                  <IconButton 
+                    colorScheme='teal' 
+                    icon={<FaPlus />} 
+                    onClick={() => setNumPortions(numPortions + 1)}
+                    isRound
+                  />
+                </HStack>
+
          <Table variant="simple">
             <Thead>
                <Tr>
@@ -25,7 +60,7 @@ export default function IngredientTable({ ingredients }) {
                {ingredients.map((ingredient) => (
                   <Tr key={ingredient.name}>
                      <Td>{ingredient.name}</Td>
-                     <Td isNumeric>{ingredient.quantity}</Td>
+                     <Td isNumeric>{ingredient.quantity * numPortions}</Td>
                      <Td>{ingredient.unit}</Td>
                   </Tr>
                ))}
